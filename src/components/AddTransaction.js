c2e1d41e323416b6fed4a87a5c7bd81114095e86
Rob Peschke <rspesc31@gmail.com>
2020-09-02 10:57:44 -0400

    added global state context

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -13,8 +13,9 @@ const AddTransaction = () => {
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
         </div>
         <div className="form-control">
-          <label htmlFor="amount"
-          >Amount <br />
+          <label htmlFor="amount">
+            Amount
+            <br />
             (negative - expense, positive - income)</label
           >
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
